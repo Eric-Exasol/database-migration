a833ca4de5bb0730b904ee515924f3d451376b76
Eric Lloyd <eric.lloyd@exasol.com>
2018-07-17 09:42:37 +0200

    test travis ci

--- testing_files/test_mysql.sh
+++ testing_files/test_mysql.sh
@@ -14,7 +14,6 @@ echo $MY_MESSAGE
 #setting up a mysql db image in docker
 docker pull mysql:5.7.22
 docker run --name mysqldb -p 3360:3360 -e MYSQL_ROOT_PASSWORD=mysql -d mysql:5.7.22
-docker logs -f mysqldb
 
 #setting up an exasol db image in docker
 docker pull exasol/docker-db:latest
