a03e196b2fc6807d4741f5336aedee3a87801177
Eric Lloyd <eric.lloyd@exasol.com>
2018-07-19 09:00:24 +0200

    test travis ci 2

--- testing_files/test_mysql.sh
+++ testing_files/test_mysql.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 MY_MESSAGE="Test message!"
 echo $MY_MESSAGE
 
@@ -15,7 +15,10 @@ set -e
 
 #setting up a mysql db image in docker
 docker pull mysql:5.7.22
-docker run --name mysqldb -p 3360:3360 -e MYSQL_ROOT_PASSWORD=mysql -d mysql:5.7.22
+docker run --name mysqldb -p 3360:3306 -e MYSQL_ROOT_PASSWORD=mysql -d mysql:5.7.22
+
+docker inspect mysqldb | grep "IPAddress"
+
 
 #setting up an exasol db image in docker
 docker pull exasol/docker-db:latest
@@ -31,15 +34,23 @@ docker cp testing_files/mysql_datatypes_test.sql mysqldb:/tmp/
 #execute the file inside the mysqldb container
 docker exec -ti mysqldb sh -c "mysql < tmp/mysql_datatypes_test.sql -pmysql"
 
+#find the ip address of the mysql container
+ip_addr_long="$(docker inspect mysqldb | grep "IPAddress")"
+len=${#ip_addr_long}
+start=$(expr $len - 12)
+ip=${ip_addr_long:$start:10}
 
-#access the mysqldb container from the exasoldb container
-# TODO get the connection string of the mysql container
-# TODO get the connection string of the exasoldb container
+#creates a file create_conn.sql with connection from the exasoldb to the mysqldb container
+echo "create or replace connection mysql_conn to 'jdbc:mysql://$ip:3306' user 'root' identified by 'mysql';" > testing_files/create_conn.sql
+#copy .sql file to be executed inside container
+docker cp testing_files/create_conn.sql exasoldb:/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/test/
+#execute the file inside the exasoldb container
+docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/exaplus  -c "127.0.0.1:8888" -u sys -p exasol -f "usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/test/create_conn.sql" -x"
+	
 
 #copy .sql file to be executed inside container
-docker cp testing_files/playground.sql exasoldb:/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/test/ &&
+docker cp my_sql_to_exasol_v2.sql exasoldb:/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/test/ &&
 #execute the file inside the exasoldb container
-docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/exaplus  -c "127.0.0.1:8888" -u sys -p exasol -f "usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/test/playground.sql" -x"
-
-MY_MESSAGE="Done!"
-echo $MY_MESSAGE
+docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/exaplus  -c "127.0.0.1:8888" -u sys -p exasol -f "usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/test/my_sql_to_exasol_v2.sql" -x" &&
+#execute the output.sql file created inside the exasoldb container
+docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/exaplus  -c "127.0.0.1:8888" -u sys -p exasol -f "output.sql" -x"
