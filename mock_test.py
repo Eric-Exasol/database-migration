22cce0a7eff6e21a40f12e52891622c35290ae70
Eric Lloyd <eric.lloyd@exasol.com>
2018-07-24 13:44:13 +0200

    test travis python

--- mock_test.py
+++ mock_test.py
@@ -1,6 +1,6 @@
 import exasol as E
 import os
 
-C = E.connect(Driver = '/home/travis/build/Eric-Exasol/database-migration/EXASOL_ODBC-6.0.2/lib/linux/x86_64/libexaodbc-uo2214lv1.so', EXAHOST = os.environ['ODBC_HOST'], EXAUID = os.environ['EXAUSER'], EXAPWD = os.environ['EXAPW'])
+C = E.connect(dsn='EXAODBC_TEST')
 R = C.readData("SELECT 'connection works' FROM dual")
 print(R)
