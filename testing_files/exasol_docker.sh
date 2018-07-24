f2a082a18afa0f05b1fe2549974f1c5845be33bd
Eric Lloyd <eric.lloyd@exasol.com>
2018-07-24 15:56:03 +0200

    test travis python

--- testing_files/exasol_docker.sh
+++ testing_files/exasol_docker.sh
@@ -14,8 +14,9 @@ docker logs -f exasoldb &
 (docker logs -f --tail 0 exasoldb &) 2>&1 | grep -q -i 'stage4: All stages finished'
 sleep 30
 
+exa_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' exasoldb)"
 docker cp generate_script.sql exasoldb:/
-docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/exaplus  -c "127.0.0.1:8888" -u sys -p exasol -f "generate_script.sql" -x"
+docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.10/bin/Console/exaplus  -c "$exa_ip:8888" -u sys -p exasol -f "generate_script.sql" -x"
 
 #./testing_files/test_oracle.sh;
 #./testing_files/test_mysql.sh;
