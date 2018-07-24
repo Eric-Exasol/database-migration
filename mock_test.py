54d34ead993c93904e6f528fdfdfc3d8795936ce
Eric Lloyd <eric.lloyd@exasol.com>
2018-07-24 12:23:43 +0200

    test travis python

--- mock_test.py
+++ mock_test.py
@@ -1,6 +1,7 @@
 import exasol as E
 import os
 
-C = E.connect(Driver = os.environ['EXAODBC'], EXAHOST = os.environ['ODBC_HOST'], EXAUID = 'sys', EXAPWD = 'exasol')
+print(os.environ['EXAODBC'] + os.environ['ODBC_HOST']
+C = E.connect(Driver = os.environ['EXAODBC'], EXAHOST = '127.0.0.1:8888', EXAUID = 'sys', EXAPWD = 'exasol')
 R = C.readData("SELECT 'connection works' FROM dual")
 print(R)
