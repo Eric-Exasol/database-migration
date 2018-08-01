71b49bf6ad5dd7897b2ed80eb1ef0a0e2a0c96f2
Eric Lloyd <eric.lloyd@exasol.com>
2018-08-01 17:36:54 +0200

    try fix exaplus version

--- test/testing_files/exasol_docker.sh
+++ test/testing_files/exasol_docker.sh
@@ -13,11 +13,11 @@ docker run --name exasoldb -p 127.0.0.1:8899:8888 --detach --privileged --stop-t
 (docker logs -f --tail 0 exasoldb &) 2>&1 | grep -q -i 'stage4: All stages finished'
 sleep 60
 
-EXAPLUS_PATH="$(docker exec -it exasoldb sh -c "find / -iname 'exaplus' 2> /dev/null")"
+EXAPLUS_PATH=$(docker exec -it exasoldb sh -c "find / -iname 'exaplus' 2> /dev/null")
 echo $EXAPLUS_PATH
 echo "test"
 #copy the generate_script.sql file
 docker cp test/generate_script.sql exasoldb:/
 #execute the generate_script.sql file which creates a script inside the exasoldb container
 #docker exec -ti exasoldb sh -c "/usr/opt/EXASuite-6/EXASolution-6.0.11/bin/Console/exaplus -c "127.0.0.1:8888" -u sys -p exasol -f "generate_script.sql" -x"
-docker exec -ti exasoldb sh -c "${EXAPLUS_PATH} -c '127.0.0.1:8888' -u sys -p exasol -f 'generate_script.sql' -x"
+docker exec -ti exasoldb sh -c "$EXAPLUS_PATH -c '127.0.0.1:8888' -u sys -p exasol -f 'generate_script.sql' -x"
