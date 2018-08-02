79fce7a53cf2715d0a15ee08ef90d6b94caffcaf
Eric Lloyd <eric.lloyd@exasol.com>
2018-08-02 10:45:55 +0200

    more testing fix

--- test/testing_files/exasol_docker.sh
+++ test/testing_files/exasol_docker.sh
@@ -5,13 +5,13 @@ echo $MY_MESSAGE
 set -e
 
 #setting up an exasol db image in docker
-#docker pull exasol/docker-db:latest
+docker pull exasol/docker-db:latest
 # to run locally : docker run --name exasoldb -p 8899:8888 --detach --privileged --stop-timeout 120  exasol/docker-db:latest
-#docker run --name exasoldb -p 127.0.0.1:8899:8888 --detach --privileged --stop-timeout 120  exasol/docker-db:latest
+docker run --name exasoldb -p 127.0.0.1:8899:8888 --detach --privileged --stop-timeout 120  exasol/docker-db:latest
 
 # Wait until database is ready
 #(docker logs -f --tail 0 exasoldb &) 2>&1 | grep -q -i 'stage4: All stages finished'
-#sleep 60
+sleep 60
 
 #copy the generate_script.sql file
 docker cp test/generate_script.sql exasoldb:/
